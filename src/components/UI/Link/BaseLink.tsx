6a57115add68d4773e2704a0c0a631f310ca3a66
lev <levovskiiy@yandex.ru>
2023-02-27 04:19:46 +0300

    fet: base-link component

--- src/components/UI/Link/BaseLink.tsx
+++ src/components/UI/Link/BaseLink.tsx
@@ -1,19 +1,26 @@
-import React, { useMemo, type FC, type HTMLAttributes } from 'react'
+import React, { type ReactNode, useMemo, type FC, type HTMLAttributes } from 'react'
+import { Link } from 'react-router-dom'
 import { classess } from '../../../utils/utils'
 
 import './BaseLink.css'
 
 interface LinkProps extends HTMLAttributes<HTMLAnchorElement> {
-  href: string
+  to: string
+  isRoute?: boolean
+  children: ReactNode
 }
 
-const BaseLink: FC<LinkProps> = ({ children, href, className, ...props }) => {
+const BaseLink: FC<LinkProps> = ({ children, to, isRoute = false, className, ...props }) => {
   const styles = ['base-link', className]
 
   const cls = useMemo(() => classess(...styles), [styles])
 
+  if (isRoute) {
+    return <Link to={to} className={cls}>{children}</Link>
+  }
+
   return (
-    <a href={href} className={cls} {...props}>
+    <a {...props} href={to} className={cls}>
       {children}
     </a>
   )
