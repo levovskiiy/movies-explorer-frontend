4c01320a4417b6e24728cc56d70ba66be3bf4ab4
lev <levovskiiy@yandex.ru>
2023-03-07 15:10:30 +0500

    fix about me section

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,7 +2,6 @@ import { BaseLink, DeleteButton, Heading, SaveButton } from 'components/UI'
 import useHover from 'hooks/useHover'
 import React, { useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-// import { useLocation } from 'react-router-dom'
 import { type IMovie } from 'types/types'
 import { classname, formatDuration } from 'utils/utils'
 
