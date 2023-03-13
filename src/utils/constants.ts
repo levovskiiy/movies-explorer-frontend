ba4ba0ffb72c64c5da7062ce77649a6ee708546d
lev <levovskiiy@yandex.ru>
2023-03-13 05:17:42 +0500

    fix: refactoring

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,10 +5,3 @@ export const API_HOST = NODE_ENV === 'development' ? 'localhost' : REACT_APP_DOM
 export const API_PORT = NODE_ENV === 'development' ? 5000 : null
 export const BEATFILM_MOVIE_API_URL = 'https://api.nomoreparties.co'
 export const API_URL = `${PROTOCOL}://${API_HOST}${API_PORT ? ':' + API_PORT : ''}`
-
-export const API_ERRORS_MESSAGES = {
-  UNAUTHORIZED: 'Вы не авторизованы',
-  INVALID_CREDENTIALS: 'Неверный логин или пароль',
-  INTERNAL_SERVER_ERROR: 'Что-то произошло на сервере. Попробуйте перезагрузить страницу и сделать запрос снова.',
-  EMAIL_ALREADY_EXIST: 'Пользователь с таким Email уже существует'
-}
